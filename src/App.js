11210b12b318ae35a81f10ae62b7118f2ac16df7
Kanagavarshini Sathiyanarayanan <kanagavarshini@gmail.com>
2018-11-06 10:22:20 -0700

    updated sidebar with hamburger icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 import './App.css';
+
 import axios from 'axios';
 
+
 class App extends Component {
-  componentDidMount(){
-    this.getVenues();
-    
+
+  //setting state for venues and markers for app component
+  state ={
+    venues: [],
+    markers: [],
+    hamburgerActive: false
   }
+
+  //when the react component monunts, it will call loadMap function
+  componentDidMount(){  
+    window.gm_authFailure = () => {
+      alert('ERROR!! \nFailed to get Google map.')
+      console.log('ERROR!! \nFailed to get Google map.')
+   }
+    this.getVenues()
+  }
+
   loadMap = () => {
     //this will load the script
     loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDDLHLqfuBmKtnjQfjeg3Sowxr39Gz0KHU&callback=initMap');
@@ -15,7 +30,7 @@ class App extends Component {
     window.initMap = this.initMap;
 
   }
-  //Get the data from foursquare api
+  // Get the FourSquare data
   getVenues = () => {
     const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
     const parameters = {
@@ -30,7 +45,6 @@ class App extends Component {
 
     axios.get(endPoint + new URLSearchParams(parameters))
     .then(response => {
-      
      this.setState({
       venues: response.data.response.groups[0].items 
     },this.loadMap())
@@ -41,74 +55,178 @@ class App extends Component {
       alert("Sorry, That didn't work. Check console for more details"); 
    })
   }
-  initMap = () => {
-    var myLatLng = {lat: 33.7, lng: -117.7675};
+ 
+  
 
-    // Create a map object and specify the DOM element
-    // for display.
-    var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: myLatLng,
-      zoom: 10
-    });
-    //intializing map
-    this.map = map;
-    //create infowindow
-    let infowindow = new window.google.maps.Infowindow();
 
-    
-    this.setState({
-      map:map,
-      infowindow: infowindow
-    });
+//constructor function will a new map
+initMap = () => {
 
-    this.state.venues.forEach(myVenue => {
-      var contentString = `${myVenue.venue.name + ","+
-      myVenue.venue.location.city+","+myVenue.venue.location.address}`
-      //creating info window
-      var infowindow =  new window.google.maps.Infowindow ({
-        content: contentString
-      });
+  // display google map
+  let map = new window.google.maps.Map(document.getElementById("map"), {
+    //set Latitude and longitude
+    center: { lat: 33.7, lng: -117.7675 },
+    //setting zoom level for map
+    zoom: 10
+  });
 
-      var marker = new window.google.maps.Marker({
-      position : {lat: myVenue.venue.location.lat, lng:myVenue.venue.location.lng},
-      map: map , 
-      city: myVenue.venue.location.city,
-      address: myVenue.venue.location.address,
-      myVenue: myVenue,
-      id: myVenue.venue.id,
-      name: myVenue.venue.name,
+  //initializing map
+  this.map = map;
+
+  //create an infowindow(https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple)
+  let infowindow = new window.google.maps.InfoWindow();
+  
+  const allMarkers = [];
+  //setting state of map and infowindow
+  this.setState({
+    map: map,
+    infowindow: infowindow
+  });
+
+  //for each value do the following(create markers with name, city and location)
+  this.state.venues.forEach(myvenue => {
+    //getting value for name, city and location and storing that in contentString variable
+    let contentString = `${myvenue.venue.name +
+      ", " +
+      myvenue.venue.location.city + ", "+ myvenue.venue.location.address}`;
+    //loop over state
+
+    //create a marker 
+    //from https://developers.google.com/maps/documentation/javascript/markers
+    let marker = new window.google.maps.Marker({
+      position: { 
+        lat: myvenue.venue.location.lat,
+        lng: myvenue.venue.location.lng
+      },
+      map: map,
+      city:myvenue.venue.location.city,
+      address: myvenue.venue.location.address,
+      myvenue: myvenue,      
+      id: myvenue.venue.id,
+      name: myvenue.venue.name,
       draggable: true,
-      animation: window.google.maps.Animation.drop,
-      title: myVenue.venue.name
+      animation: window.google.maps.Animation.DROP,
+      title: myvenue.venue.name
     });
-    marker.addListener('click',function(){
-        
-      //update content before opening infowindow
+
+    //setting event listener for marker
+    marker.addListener("click", function() {
+      
+      //change content before opening infowindow and set that to contentString which has information about name city and address
       infowindow.setContent(contentString);
-      //this function will be invoked to open infowindow
-      infowindow.open(map,marker);
+
+      //this function will be called to open infowindow
+      infowindow.open(map, marker);
     });
-    
+    //push marker
+    allMarkers.push(marker);
+  });
 
+  this.setState({
+    markers: allMarkers
+  });
+  this.setState({ filtermyvenue: this.state.venues });
+};
 
-  }); 
- }     
+constructor(props) {
+  super(props);
+  this.state = {
+    sidebar: false,
+    query: ""
+  };
+}
+
+// Toggle sidebar
+  onHamburgerClick = () => {
+    const sidebar = document.querySelector('sidebar');
+    if (this.state.hamburgerActive) {
+      //sidebar.styles.transform = 'translateX(-250px)'
+      sidebar.styles.display="none";
+      
+      this.setState({ hamburgerActive: false });
+    }
+    else {
+      //sidebar.styles.transform = 'translateX(0px)'
+      sidebar.styles.display="block";
+      this.setState({sidebar: true});
+      this.setState({ hamburgerActive: true });
+    }
+  }
+listItemClick = (venues) => {
+  //This will check if the marker id match the venue id 
+  let marker = this.state.markers.filter(m => m.id === venues.id)[0];
+
+//adding city and address on click of list item
+  this.state.infowindow.setContent(`${marker.name +
+    ", " +
+  marker.city + ", " + marker.address}`);
+
+  //set the map positon to marker positon
+  this.map.setCenter(marker.position);
+  //open infowindow
+  this.state.infowindow.open(this.state.map, marker);
+  
+}
+
+//filtering venues to match the query 
+filtermyvenue(query) {
+  let f = this.state.venues.filter(myvenue => myvenue.venue.name.toLowerCase().includes(query.toLowerCase()))
+  
+  //show the infowindow 
+  this.state.markers.forEach(marker => {
+    //if marker matches the query set marker visibility true else false
+    marker.name.toLowerCase().includes(query.toLowerCase()) === true ?
+    marker.setVisible(true) :
+    marker.setVisible(false);
+  });
+  //when there is no result found in query close the infowindow
+  if (f.length === 0) {
+    //this will close the infowindow  
+    this.state.infowindow.close();   
+  }
+  //Set the state to reflect the query
+  this.setState({filtermyvenue: f, query}); 
+}
+
   render() {
-    return (
+    return (  
       <main>
-        <div id="map"></div>
-      </main>  
-    );
+        <button aria-label='Hamburger Menu' tabIndex='0' className="hamburger-container" onClick={this.onHamburgerClick}> 
+          <div className="hamburger-bar"></div>
+          <div className="hamburger-bar"></div>
+          <div className="hamburger-bar"></div>
+        </button>
+        <div className="header"><strong>Pizza points- Orange County, CA</strong></div>     
+        <div role="application" aria-label="map" id='map'></div>
+        
+          <div aria-label="sidebar" id='sidebar'>
+          
+          
+          
+            <input type="text" autoFocus="autofocus" tabIndex="0" className="SearchVenues" placeholder="Search venues" value={this.state.query} onChange={(e)=>{this.filtermyvenue(e.target.value)}}/>
+            <br/>
+            <br/>
+            {
+              this.state.filtermyvenue && this.state.filtermyvenue.length > 0 && this.state.filtermyvenue.map((myvenue, index) => (
+                  <div tabIndex="-1" key={index} className="venue-item">
+                      {/* <h4>{myvenue.venue.name}</h4> */}
+                      <button onClick={()=>{this.listItemClick(myvenue.venue)}}>{myvenue.venue.name}</button>
+                  </div>
+              ))
+            }
+        </div>
+      </main>      
+    )
   }
 }
 
-function loadScript(url){
+function loadScript(source) {
   try {
     //adding try catch block to make sure if script doesnt work it gives proper console and alert
     //select script tag
     var index = window.document.getElementsByTagName('script')[0]
     var script = window.document.createElement('script')
-    script.src = url
+    script.src = source
     script.async = true
     script.defer = true
     index.parentNode.insertBefore(script, index)
@@ -118,7 +236,5 @@ function loadScript(url){
    alert("Sorry, That didn't work. Check console for more details");
    }
   }
-
-
 
 export default App;
